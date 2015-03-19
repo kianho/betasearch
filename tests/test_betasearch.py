e15f0500ec171c1a8e9fc4e5126e616651982005
Kian Ho <hui.kian.ho@googlemail.com>
2015-03-19 19:42:20 +1100

    Added more test cases to test_betasearch.py.

--- tests/test_betasearch.py
+++ tests/test_betasearch.py
@@ -9,12 +9,35 @@ class test_betasearch(unittest.TestCase):
         pass
 
     def test_Elbow(self):
-        elbow_A = betasearch.Elbow(1, 2)
-        elbow_B = betasearch.Elbow(1, 2)
-        elbow_C = betasearch.Elbow(2, 1)
+        from betasearch import Elbow
+
+        elbow_A = Elbow(1, 2)
+        elbow_B = Elbow(1, 2)
+        elbow_C = Elbow(2, 1)
 
         self.assertTrue(elbow_A.same(elbow_B))
         self.assertFalse(elbow_A.same(elbow_C))
+
+        self.assertRaises(ValueError, lambda : Elbow(-1, -1))
+        self.assertRaises(ValueError, lambda : Elbow(-1, 0))
+        self.assertRaises(ValueError, lambda : Elbow(-1, 2))
+
+    def test_Span(self):
+        from betasearch import Span
+
+        span_A = Span(1, 2)
+        span_B = Span(1, 2)
+        span_C = Span(2, 1)
+
+        self.assertTrue(span_A.same_direction(span_B))
+        self.assertFalse(span_A.same_direction(span_C))
+
+        self.assertTrue(span_A.overlaps(span_B))
+        self.assertTrue(span_A.overlaps(span_C))
+
+        self.assertRaises(ValueError, lambda : Span(0, 0))
+        self.assertRaises(ValueError, lambda : Span(-1, 0))
+        self.assertRaises(ValueError, lambda : Span(-1, 2))
 
 if __name__ == "__main__":
     unittest.main()
